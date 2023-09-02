750a9e7527d87b4c7cf4782584fcde4424d272c1
Satish Marisetty <satishmarisetti79@gmail.com>
2023-09-02 21:41:50 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,99 +10,36 @@ class xyloApp extends StatelessWidget {
     final player = AudioPlayer();
     player.play(AssetSource('note$sel.wav'));
   }
+  Widget block(Color colour,int sel)
+  {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(padding: EdgeInsets.all(0)),
+        child: Container(
+          color: colour,
+        ),
+        onPressed: ()
+        {
+          playSound(sel);
+        },
+      ),
+    );
+  }
   const xyloApp({super.key});
   @override
   Widget build(BuildContext context) {
-    int sel=1;
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Column(
-            children: [
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(padding: EdgeInsets.all(0)),
-                child: Container(
-                  color: Colors.yellow,
-                ),
-                onPressed: ()
-                  {
-                    playSound(1);
-                  },
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(padding: EdgeInsets.all(0)),
-                  child: Container(
-                    color: Colors.blue,
-                  ),
-                  onPressed: ()
-                  {
-                    playSound(2);
-                  },
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(padding: EdgeInsets.all(0)),
-                  child: Container(
-                    color: Colors.pinkAccent,
-                  ),
-                  onPressed: ()
-                  {
-                    playSound(3);
-                  },
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(padding: EdgeInsets.all(0)),
-                  child: Container(
-                    color: Colors.orange,
-                  ),
-                  onPressed: ()
-                  {
-                    playSound(4);
-                  },
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(padding: EdgeInsets.all(0)),
-                  child: Container(
-                    color: Colors.redAccent,
-                  ),
-                  onPressed: ()
-                  {
-                    playSound(5);
-                  },
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(padding: EdgeInsets.all(0)),
-                  child: Container(
-                    color: Colors.greenAccent,
-                  ),
-                  onPressed: ()
-                  {
-                    playSound(6);
-                  },
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(padding: EdgeInsets.all(0)),
-                  child: Container(
-                    color: Colors.indigoAccent,
-                  ),
-                  onPressed: ()
-                  {
-                    playSound(7);
-                  },
-                ),
-              ),
+            children: <Widget>[
+             block(Colors.amber,1),
+             block(Colors.blue,2),
+             block(Colors.pinkAccent,3),
+             block(Colors.orange,4),
+             block(Colors.red,5),
+             block(Colors.green,6),
+             block(Colors.yellowAccent,7),
             ],
           ),
         ),
@@ -110,5 +47,3 @@ class xyloApp extends StatelessWidget {
     );
   }
 }
-
-
